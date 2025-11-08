03a7204828707efdd55c941a859a0738f15da8d2
FelixOrban <felixorban123@gmail.com>
2025-11-08 19:03:37 +0200

    Add project files.

---
+++ (1) nNumbersEven/(1) nNumbersEven/Program.cs
@@ -0,0 +1,28 @@
+﻿//Se da o secventa de n numere. Sa se determine cate din ele sunt pare.
+
+namespace nNumbersEven;
+
+using System;
+using System.Linq;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Type any numbers between spaces(ex: 1 2 3...) : ");
+        string input = Console.ReadLine() ?? string.Empty;
+        int count = 0;
+
+        foreach (var numString in input.Split(' ', StringSplitOptions.RemoveEmptyEntries))
+        {
+            if (int.TryParse(numString, out int numInt))
+            {
+                if (numInt % 2 == 0)
+                    count++;
+            }
+            else
+                Console.WriteLine($"'{numString}' is not a valid number.");
+        }
+        Console.WriteLine($"There is {count} even numbers");
+    }
+}
---
+++ (2) nNegZerPoz/(2) nNegZerPoz/Program.cs
@@ -0,0 +1,36 @@
+﻿//Se da o secventa de n numere. Sa se determina cate sunt negative, cate sunt zero si cate sunt pozitive. 
+
+namespace nNegZerPoz;
+
+using System;
+using System.Linq;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Type in any numbers with spaces between them(ex: 1 2 3...) : ");
+        string input = Console.ReadLine() ?? string.Empty;
+        int countNeg = 0;
+        int countZer = 0;
+        int countPoz = 0;
+
+        foreach (var numString in input.Split(' ' , StringSplitOptions.RemoveEmptyEntries))
+        {
+            if (int.TryParse(numString, out int numInt))
+            {
+                if(numInt<0)
+                    countNeg++;
+                else if(numInt>0)
+                    countPoz++;
+                else
+                    countZer++;
+            }
+            else
+                Console.WriteLine($"'{numString}' is not a valid number");
+        }    
+        Console.WriteLine($"Negative numbers: {countNeg}");
+        Console.WriteLine($"Zero numbers: {countZer}");
+        Console.WriteLine($"Positive numbers: {countPoz}");
+    }
+}
