b0b6f67e88e9d90a7413e371ccfa35c4f768960a
FelixOrban <felixorban123@gmail.com>
2025-11-08 19:12:06 +0200

    a

---
+++ (3) nSumProd/(3) nSumProd/Program.cs
@@ -0,0 +1,25 @@
+﻿//Calculati suma si produsul numerelor de la 1 la n. 
+
+namespace nSumProd;
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("n : ");
+        int n = int.Parse(Console.ReadLine());
+        int sum = 0;
+        int prod = 0;
+
+        if (n > 0) prod++;
+        for (int i = 1; i <= n; ++i)
+        {
+            sum += i;
+            prod *= i;
+        }
+
+        Console.WriteLine($"Sum = {sum}");
+        Console.WriteLine($"Product = {prod}");
+    }
+}
