bb9eb568bb40d8a2a93bc168c670e4a472f71c10
FelixOrban <felixorban123@gmail.com>
2025-11-09 19:01:53 +0200

    a

---
+++ (7) nLowHigh/(7) nLowHigh/Program.cs
@@ -0,0 +1,39 @@
+﻿//Se da o secventa de n numere. Sa se determine cea mai mare si cea mai mica valoare din secventa. 
+
+namespace nLowHigh;
+using System;
+using System.Linq;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Type in any numbers between spaces (ex : 1 2 3 ... ) : ");
+        string input = Console.ReadLine() ?? string.Empty;
+
+        bool firstnum = true;
+        int lowest = 0;
+        int highest = 0;
+
+        foreach (var numString in input.Split(' ', StringSplitOptions.RemoveEmptyEntries))
+        {
+            if (int.TryParse(numString, out int num))
+            {
+                if (firstnum) // just for the first number
+                {
+                    lowest = num;
+                    highest = num;
+                    firstnum = false;
+                }
+                else if (num < lowest)
+                    lowest = num;
+                else
+                    highest = num;
+            }
+            else
+                Console.WriteLine($"'{numString}' is not a valid");
+        }
+        Console.WriteLine($"Lowest number: {lowest}");
+        Console.WriteLine($"Highest number: {highest}");
+    }
+}
---
+++ (8) nFibonacci/(8) nFibonacci/Program.cs
@@ -0,0 +1,39 @@
+﻿//Determianti al n-lea numar din sirul lui Fibonacci. Sirul lui Fibonacci se construieste astfel: f1 = 0, f2 = 1, f_n = f_(n-1) + f(n-2). Exemplu: 0, 1, 1, 2, 3, 5, 8 ...
+
+namespace nFibonacci;
+using System;
+using System.Linq;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("n : ");
+        int n = int.Parse(Console.ReadLine());
+        if (n <= 0)
+        {
+            Console.WriteLine("'n' has to be greater than 0");
+            return;
+        }
+
+        long fibN = Fibonacci(n);
+        Console.WriteLine($"the {n}. number in the fibonacci line is : {fibN}");
+    }
+    static long Fibonacci(int n)
+    {
+        if (n == 1) return 0;
+        if (n == 2) return 1;
+
+        long prev = 0;
+        long curr = 1;
+
+        for (int i = 3; i <= n; i++)
+        {
+            long next = prev + curr; //Fi = F(i-1) + F(i-2)
+            prev = curr; 
+            curr = next;
+        }
+        return curr;
+    }
+
+}
